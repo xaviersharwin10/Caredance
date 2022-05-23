19e90283c6e84a7b47d24524a4468aaac85ce63b
sharwinxavier <65103675+sharwinxavier@users.noreply.github.com>
2022-05-23 20:41:56 +0530

    second commit

--- lib/career_path.dart
+++ lib/career_path.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const Career_Path());
@@ -68,113 +68,113 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     '12',
   ];
 
+  var temps = [
+    'Software Engineering',
+    'Mechanical Engineering',
+    'Finance',
+    'Human Resources',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder(
+      body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('Streams').snapshots(),
-    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-    if (!snapshot.hasData) {
-    return Center(
-    child: CircularProgressIndicator(),
-    );
-    }
-    if (!snapshot.hasData) {
-    temp = snapshot.data!.docs[0].id;
-    // debugPrint('setDefault make: $temp');
-    }
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (!snapshot.hasData) {
+            temp = snapshot.data!.docs[0].id;
+            // debugPrint('setDefault make: $temp');
+          }
 
-    return Form(
-    key: _formKey,
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
+          return Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 3),
+                Text(
+                  "Select your field of Interest",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                const SizedBox(height: 3),
+                DropdownButton(
+                  // Initial Value
+                  value: temp,
 
-    const SizedBox(height: 3),
-    Text(
-    "Select your field of Interest",
-    style: TextStyle(fontSize: 20.0),
+                  // Down Arrow Icon
+                  icon: const Icon(Icons.keyboard_arrow_down),
 
-    ),
-    const SizedBox(height: 3),
-    DropdownButton(
+                  // Array list of items
+                  items: snapshot.data!.docs.map((value) {
+                    return DropdownMenuItem(
+                      value: value.id,
+                      child: Text('${value.id}'),
+                    );
+                  }).toList(),
+                  // After selecting the desired option,it will
+                  // change button value to selected value
+                  onChanged: (value) {
+                    setState(() {
+                      temp = value!;
+                    });
+                  },
+                ),
+                const SizedBox(height: 3),
+                Text(
+                  "Grade",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                const SizedBox(height: 3),
+                DropdownButton(
+                  // Initial Value
+                  value: dropdownvalue,
+                  // controller: dropdowncontroller,
+                  // Down Arrow Icon
+                  icon: const Icon(Icons.keyboard_arrow_down),
 
-    // Initial Value
-    value: temp,
-
-    // Down Arrow Icon
-    icon: const Icon(Icons.keyboard_arrow_down),
-
-    // Array list of items
-    items: snapshot.data!.docs.map((value) {
-    return DropdownMenuItem(
-    value: value.id,
-    child: Text('${value.id}'),
-    );
-    }).toList(),
-    // After selecting the desired option,it will
-    // change button value to selected value
-    onChanged: (value) {
-    setState(() {
-    temp = value!;
-    });
-    },
-    ),
-    const SizedBox(height: 3),
-    Text(
-    "Grade",
-    style: TextStyle(fontSize: 20.0),
-
-    ),
-    const SizedBox(height: 3),
-
-    DropdownButton(
-
-    // Initial Value
-    value: dropdownvalue,
-    // controller: dropdowncontroller,
-    // Down Arrow Icon
-    icon: const Icon(Icons.keyboard_arrow_down),
-
-    // Array list of items
-    items: grades.map((String items) {
-    return DropdownMenuItem(
-    value: items,
-    child: Text(items),
-    );
-    }).toList(),
-    // After selecting the desired option,it will
-    // change button value to selected value
-    onChanged: (String? newValue) {
-    setState(() {
-    dropdownvalue = newValue!;
-    });
-    },
-    ),
-    const SizedBox(height: 3),
-
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0),
-        child: ElevatedButton(
-          onPressed: () {
-            // Validate will return true if the form is valid, or false if
-            // the form is invalid.
-            if (_formKey.currentState!.validate()) {
-              // Process data.
-              _sendDataToSecondScreen(context);
-            }
-          },
-          child: const Text('Submit'),
-        ),
+                  // Array list of items
+                  items: grades.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(items),
+                    );
+                  }).toList(),
+                  // After selecting the desired option,it will
+                  // change button value to selected value
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownvalue = newValue!;
+                    });
+                  },
+                ),
+                const SizedBox(height: 3),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState!.validate()) {
+                        // Process data.
+                        _sendDataToSecondScreen(context);
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
-    ],
-    ),
-    );
-    }
-          ,
-        ),
     );
   }
+
   void _sendDataToSecondScreen(BuildContext context) {
     // String textToSend = textFieldController.text;
     Navigator.push(
@@ -182,92 +182,146 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         MaterialPageRoute(
           builder: (context) => SecondScreen(text1: temp),
         ));
-        // );
+    // );
   }
 }
 
-
-
-
-
 class SecondScreen extends StatefulWidget {
-
   const SecondScreen({Key? key, required this.text1}) : super(key: key);
-  final String ?text1;
+  final String? text1;
 
   // var temp4;
 
   @override
   State<SecondScreen> createState() => _SecondScreenState();
 }
+
 class _SecondScreenState extends State<SecondScreen> {
-  var temp4;
+  // List<String> pointList = <String>[];
+  // getdata() async{
+  //   await FirebaseFirestore.instance.collection("points").doc('biZV7cepFJA8T6FTcF08').get().then((value){
+  //     setState(() {
+  //       // first add the data to the Offset object
+  //       List.from(value.data['point']).forEach((element){
+  //         String data = new String(element);
+  //
+  //         //then add the data to the List<Offset>, now we have a type Offset
+  //         pointList.add(data);
+  //       });
+  //
+  //     });
+  //   });
+  // }
+  var temp4='One';
   var temp5 = <String>[];
+  var lenth1;
+  List<DropdownMenuItem> subdomains = [];
+  List<String> subdomains1 = [];
+
   // var db = firebase.firestore();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('Streams').where('sub-domain-of', isEqualTo: widget.text1).snapshots(),
-    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-    if (!snapshot.hasData) {
-      debugPrint('IT has data');
-      // debugPrint(snapshot.data!.docs[0].get('sub-domains'));
-    return Center(
-    child: CircularProgressIndicator(),
-    );
+        appBar: AppBar(),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('Streams')
+                .where('sub-domain-of', isEqualTo: widget.text1)
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              DocumentSnapshot snap = snapshot.data!.docs[0];
+              lenth1 = snap.get("sub-domains").length;
+              print(lenth1);
+              for (var i = 0; i < lenth1; i++) {
+                subdomains1.add(snap.get("sub-domains")[i].toString());
+              }
+              if (!snapshot.hasData) {
+                debugPrint('IT has data');
 
-    }
-    if (!snapshot.hasData) {
-    temp4 = snapshot.data!.docs[0].get('sub-domains');
-    // temp5 = snapshot.data!.get('sub-domains');
-    // debugPrint('setDefault make: $temp4');
-    }
-    return Center(
-    child: Column(
-    children: [
-    SizedBox(
-    height: 50,
-    ),
-    // Step 4 <-- SEE HERE
-    Text(
-    '${widget.text1}',
-    style: TextStyle(fontSize: 54),
-    ),
-    DropdownButton(
+                // debugPrint(snapshot.data!.docs[0].get('sub-domains'));
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-    // Initial Value
-    value: temp4,
+              // temp4 = 'One';
+              // temp5 = snapshot.data!.get('sub-domains');
+              // debugPrint('setDefault make: $temp4');
 
-    // Down Arrow Icon
-    icon: const Icon(Icons.keyboard_arrow_down),
+              return Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    // Step 4 <-- SEE HERE
+                    Text(
+                      '${widget.text1}',
+                      style: TextStyle(fontSize: 54),
+                    ),
+                    // DropdownButton<String>(
+                    //   hint: Text("Status"),
+                    //   value: temp4,
+                    //   items: ['1','2','3']
+                    //       .map<DropdownMenuItem<String>>
+                    //     ((String value) {
+                    //     return new DropdownMenuItem<String>(
+                    //       value: value,
+                    //       child: new Text(value),
+                    //     );
+                    //   }).toList(),
+                    //   onChanged: (String? value) {
+                    //     print(value);
+                    //     setState(() {
+                    //       temp4 = value;
+                    //       // value = temp4;
+                    //     });
+                    //   },
+                    // ),
+                    DropdownButton<String>(
+                      value: temp4,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          temp4 = newValue!;
+                        });
+                      },
+                      items: snap.get()
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
 
-    // Array list of items
-    items: snapshot.data!.docs.map((value) {
-    return DropdownMenuItem(
-
-    value: value.get("sub-domains"),
-    child: Text('${value.get("sub-domains")}'),
-    );
-    }).toList(),
-    // After selecting the desired option,it will
-    // change button value to selected value
-    onChanged: (value) {
-    setState(() {
-    temp4 = value!;
-    });
-    },
-    ),
-    const SizedBox(height: 3),
-
-
-    ],
-    ),
-    );
-    }
-
-      )
-    );
+                    // DropdownButton(
+                    //   // Initial Value
+                    //   value: temp4,
+                    //
+                    //   // Down Arrow Icon
+                    //   icon: const Icon(Icons.keyboard_arrow_down),
+                    //
+                    //   // Array list of items
+                    //   items: snap.get("sub-domains").map((value) {
+                    //     return DropdownMenuItem(
+                    //       // for
+                    //       value: value,
+                    //       child: value,
+                    //     );
+                    //   }).toList(),
+                    //   // After selecting the desired option,it will
+                    //   // change button value to selected value
+                    //   onChanged: (value) {
+                    //     setState(() {
+                    //       temp4 = value!;
+                    //     });
+                    //   },
+                    // ),
+                    const SizedBox(height: 3),
+                  ],
+                ),
+              );
+            }));
   }
 }
